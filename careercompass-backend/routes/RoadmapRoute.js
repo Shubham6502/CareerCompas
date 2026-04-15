c414c3fbaa46a973fb92089b7aecea26cf02c2ee
Shubham6502 <shubhampatil6502@gmail.com>
2026-04-15 16:19:11 +0530

    remove old folders

--- careercompass-backend/routes/RoadmapRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import Roadmap from "../models/Roadmap.js";
-import { getRoadmapByDomain ,getRoadmapsByDay} from "../controllers/roadmapController.js";
-
-const router = express.Router();
-
-router.get("/:domain",getRoadmapByDomain);
-router.get("/getRoadmap/:domain",getRoadmapsByDay);
-export default router;
--- careercompass-backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { saveUser,UpdateUser,deleteUser } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post("/save", saveUser);
-router.post("/saveLogin",UpdateUser)
-router.delete("/delete/:clerkId",deleteUser);
-export default router;
