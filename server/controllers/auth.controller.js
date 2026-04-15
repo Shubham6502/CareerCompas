fab1cd2b9773618e137b12a5e02f72604b189e15
Shubham6502 <shubhampatil6502@gmail.com>
2026-04-15 17:49:06 +0530

    Resolve:cookies Issue

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -31,8 +31,8 @@ export const register = async (req, res) => {
         const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" });  
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         }); 
         await newUser.save();
