ef924885a723dbb2df8a897f771218d298f2fbea
SHUBHAM KESHAV PATIL <90743975+Shubham6502@users.noreply.github.com>
2026-06-16 20:19:07 +0530

    Change email transport port to 2525

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -194,7 +194,7 @@ export const sendOtp = async (req, res) => {
 
     const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.BREVO_USER,
